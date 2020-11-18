d2a79acf95d871ffaa42a1f5e88373851904784d
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-18 03:49:14 -0300

    Exercicio com for testado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_com_laco_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Deseja ver a tabuada de qual número?");
+           int resposta = int.Parse(Console.ReadLine());
+
+           int resultado;
+
+           for (int contador = 0; contador <= 10; contador++)
+           {
+               //código a se repetir
+                resultado = resposta * contador;
+
+               Console.WriteLine($"{resposta} x {contador} = {resultado}");
+               
+           }
+        }
+    }
+}
